610b808a1fd8380b17b1b588eac5105242e11bbe
Your Name <you@example.com>
2022-07-12 02:03:08 -0500

    creacion de funciones que detectan nombre y apellidos

--- main.c
+++ main.c
@@ -4,6 +4,12 @@
 
 void primera_letra_nom (char *stringg);
 
+const char* todo_mayusculas (char *stringg,int j);
+
+void apellido_paterno (char *stringg,int j);
+
+void apellido_materno (char *stringg,int j);
+
 main(){
     char p[30];
     int ch,i=0,f;
@@ -12,19 +18,23 @@ main(){
     printf("Please, provide input: ");
 
     while ((ch = getchar()) != '\n') {
-        p[i] = (char)ch - 32;
+        p[i] = (char)ch;
         i++;
     }
     array = (int *)malloc (i*sizeof(int));
-
     for (f = 0; f <= i; f++){
         array[f] = p[f];
     }
-    printf("%s\n",array);
-    apepaterno(array);
+    
+    printf("%s  \n",array);
+    printf("%s  \n",todo_mayusculas(array,i));
+    apellido_paterno(array,i);
+    apellido_materno(array,i);
+    primera_letra_nom(array);
     free(array);
     exit(EXIT_SUCCESS);
 }
+
 void primera_letra_nom (char *stringg){
     char *apellido;
     int j = 0,n = 0,f=0;
@@ -35,11 +45,62 @@ void primera_letra_nom (char *stringg){
         }
         else{j++;}
     }
-
-    apellido = (int *)malloc (j*sizeof(int));
+    apellido = (int *)malloc(j*sizeof(int));
     for (f = 0; f <= j; f++){
         apellido[f] = stringg[f];
         printf("%c",apellido[f]);
     }
     free(apellido);
+}
+
+const char* todo_mayusculas (char *stringg,int j){
+    int i;
+    for (i = 0; i <= j; i++){
+        if((stringg[i]>=95)&&(stringg[i]<=122)){
+            stringg[i] -= 32;
+        }
+    }
+    return stringg;
+}
+
+void apellido_paterno (char *stringg,int j){
+    int i=0,n=0,h=0;
+
+    for (i = 0; i <= j; i++){
+        if(stringg[i]== ' '){
+            n++;
+        }
+    }
+    i = 0;
+    while (n == 2){
+        if(stringg[i]== ' '){
+            printf("%c%c",stringg[i+1],stringg[i+2]);
+            break;
+        }
+        i++;
+    }
+    i = 0;
+    while (n == 3){
+        if(stringg[i]== ' '){
+            h++;
+        }
+        if (h == 2){
+            printf("%c%c",stringg[i+1],stringg[i+2]);
+            break;
+        }
+        i++;
+    }
+    
+}
+
+void apellido_materno (char *stringg,int j){
+    int i=0,n=0,h=0;
+    while (i != j){
+        if (stringg[i] == ' '){
+            n++;
+            h = i;
+        }
+        i++;
+    }
+    printf("%c",stringg[h+1]);
 }
